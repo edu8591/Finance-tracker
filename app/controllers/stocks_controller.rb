addfc4891d39f73236f5fd9ea5ac0cf92bfb3432
Eduardo Serrano <eduardoserrano8591@gmail.com>
2022-03-03 21:28:59 -0500

    Ajax implementation for retreiving stock search results

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,11 +1,20 @@
 class StocksController < ApplicationController
+
   def search
     if params[:stock].present?
       @stock = Stock.new_lookup(params[:stock])
-      flash[:alert] = 'Please enter a valid symbol to search.' unless @stock
+      if @stock
+        respond_to do |format|
+          format.js { render partial: 'shared/search_result' }
+        end
+      else
+        flash[:alert] = 'Please enter a valid symbol to search.'
+        render 'users/my_portfolio'
+      end
+
     else
       flash[:alert] = 'Please enter a symbol to search.'
+      render 'users/my_portfolio'
     end
-    render 'users/my_portfolio'
   end
 end
--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,8 @@ Bundler.require(*Rails.groups)
 
 module FinanceTracker
   class Application < Rails::Application
+    config.action_view.embed_authenticity_token_in_remote_forms = true
+
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
