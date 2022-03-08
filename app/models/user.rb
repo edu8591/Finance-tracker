c2b3ca05e2ee3fbb07cf3edfb80eb044df56f088
Eduardo Serrano <eduardoserrano8591@gmail.com>
2022-03-07 19:45:04 -0500

    Adds first and last_name field to the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,4 +21,8 @@ class User < ApplicationRecord
   def can_track_stock?(ticker)
     under_stock_limit? && !stock_already_tracked?(ticker)
   end
+
+  def full_name
+    first_name || last_name ?  "#{first_name} #{last_name}" : "Anonymous"
+  end
 end
