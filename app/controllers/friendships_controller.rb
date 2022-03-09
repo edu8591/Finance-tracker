cbab3ed5db72853f7c3262bd43c4f49b9f3239f0
Eduardo Serrano <eduardoserrano8591@gmail.com>
2022-03-09 12:13:01 -0500

    Creates partial to list friends and creates the friendships controller and create action to remove the from my_friends list

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,6 +1,9 @@
 class FriendshipsController < ApplicationController
 
-  def index
-    @friends = current_user.friends
+  def destroy
+    @friendship = Friendship.find_by(user: current_user, friend: params[:id])
+    @friendship.destroy
+    flash[:notice] = "#{@friendship.friend.full_name} was successfully removed from your friends list."
+    redirect_to my_friends_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   root 'welcome#index'
   resources :user_stocks, only: %i[create destroy]
   devise_for :users
-  resources :friendships, only: %i[index]
+  resources :friendships, only: %i[destroy]
+  # resources :friendships, only: %i[index]
+  get 'my_friends', to: 'users#my_friends'
   get 'my_portfolio', to: 'users#my_portfolio'
   get 'search_stock', to: 'stocks#search'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
