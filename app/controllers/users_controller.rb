8a52271dfe541538764f35036508408138e2130d
Eduardo Serrano <eduardoserrano8591@gmail.com>
2022-03-10 15:28:19 -0500

    show user stocks on their show page and allow for adding a friend stock to my portfolio

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
+
+  def show
+    @user = User.find(params[:id])
+    @tracked_stocks = @user.stocks
+  end
+
   def my_portfolio
     @tracked_stocks = current_user.stocks
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :user_stocks, only: %i[create destroy]
   resources :friendships, only: %i[create destroy]
+  resources :users, only: %i[show]
   root 'welcome#index'
   get 'my_friends', to: 'users#my_friends'
   get 'my_portfolio', to: 'users#my_portfolio'
